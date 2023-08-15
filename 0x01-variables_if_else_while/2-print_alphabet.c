3000e989f5cc5b6ed64a80615082738df3c92eb0
Numi-anne <annadejoro@gmail.com>
2023-08-15 04:48:03 -0700

    if positive or negative

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/* main - Entry point
+ * Description: print alphabeth in lower case
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char abc;
+
+	for (abc = 'a'; abc <= 'z'; abc++)
+	{
+		putchar("abc\n");
+	}
+	return (0)
+}
