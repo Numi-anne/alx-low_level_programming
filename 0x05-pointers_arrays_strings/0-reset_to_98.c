09d851b9d2e4563acb55c273c8cdcb3f157163bb
Numi-anne <annadejoro@gmail.com>
2023-08-21 19:56:56 -0700

    set int *n to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes pointer to int the value of 98
+ * Return - nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+learning pointers, array, and strings
