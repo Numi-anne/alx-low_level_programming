6acd744acb25d707b9c8281620214b40ae99573e
Numi-anne <annadejoro@gmail.com>
2023-08-29 20:09:30 -0700

    locating a substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strstr - Entry point
+ * @haystack: input
+ * @needle: input
+ * Return: Always 0 (Success)
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *m = haystack;
+		char *q = needle;
+
+		while (*m == *q && *q != '\0')
+		{
+			m++;
+			q++;
+		}
+
+		if (*q == '\0')
+			return (haystack);
+	}
+
+	return (0);
+}
