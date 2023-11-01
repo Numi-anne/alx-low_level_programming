ead1d6057a63623f77e49df3dac7894e2e5353c3
Numi-anne <annadejoro@gmail.com>
2023-10-31 17:03:21 -0700

    malloc_free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -7,7 +7,7 @@
  * Description: create array of size and assign char c
  *
  * Return: pointer to array, NULL if fail
- */             
+ */
 char *create_array(unsigned int size, char c)
 {
 	char *str;
@@ -17,6 +17,6 @@ char *create_array(unsigned int size, char c)
 	if (size == 0 || str == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
-                str[i] = c;
-        return (str);
+		str[i] = c;
+	return (str);
 }
